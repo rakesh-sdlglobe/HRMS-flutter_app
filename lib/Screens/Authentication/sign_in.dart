2831d8af4e0ec1fdc0efc32e475f6ada10c83e4f
Rakesh swain <142288684+rakesh-sdlglobe@users.noreply.github.com>
2024-05-13 14:02:26 +0530

    Update sign_in.dart

--- lib/Screens/Authentication/sign_in.dart
+++ lib/Screens/Authentication/sign_in.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hrm_employee/constant.dart';
+// ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../main.dart';
 import '../Home/home_screen.dart';
 import 'forgot_password.dart';
 import 'sign_up.dart';
+// ignore: depend_on_referenced_packages
+import 'package:nb_utils/nb_utils.dart';
+import '../../GlobalComponents/button_global.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _SignInState createState() => _SignInState();
 }
 
@@ -22,12 +27,33 @@ class _SignInState extends State<SignIn> {
   final TextEditingController passwordController = TextEditingController();
   bool isChecked = false;
 
+//added at 09/may/24 Wednsday
+  @override
+  void initState() {
+    super.initState();
+    checkLoggedIn(); // Check if user is already logged in on screen initialization
+  }
+
+  //added at 09/may/24 Wednsday
+  Future<void> checkLoggedIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('token');
+    if (token != null) {
+      // User is already logged in, navigate to home screen
+      Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    }
+  }
+
   Future<void> signIn() async {
     String hashedPassword = passwordController.text;
     String userID = userIDController.text;
 
     var response = await http.post(
-      Uri.parse('http://192.168.0.7:3000/auth/login'),
+      Uri.parse('http://192.168.0.24:3000/auth/login'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -43,17 +69,19 @@ class _SignInState extends State<SignIn> {
       String userID = responseData['userID'];
       await storeUserData(token, userID);
 
+      // ignore: use_build_context_synchronously
       UserData userData = context.read<UserData>();
       userData.setUserData(token, userID);
 
-
       Navigator.pushReplacement(
+        // ignore: use_build_context_synchronously
         context,
         MaterialPageRoute(
-          builder: (context) => HomeScreen(),
+          builder: (context) => const HomeScreen(),
         ),
       );
     } else {
+      // ignore: avoid_print
       print('Login failed');
     }
   }
@@ -68,140 +96,177 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        iconTheme: const IconThemeData(color: Colors.black),
-        title: Text(
-          'Sign In',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Sign In now to begin an amazing journey',
-              style: TextStyle(color: Colors.black),
+      backgroundColor: kMainColor,
+      // appBar: AppBar(
+      //   backgroundColor: kMainColor,
+      //   elevation: 0.0,
+      //   iconTheme: const IconThemeData(color: Colors.white),
+      //   title: Text(
+      //     'Sign In',
+      //     style: kTextStyle.copyWith(
+      //         color: Colors.white, fontWeight: FontWeight.bold),
+      //   ),
+      // ),
+      body: Form(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 35.0, left: 20.0, right: 20.0, bottom: 20.0),
+              // child: Text(
+              //   'Welcome to SmartH2R',
+              //   style: kTextStyle.copyWith(color: Colors.white),
+              // ),
+              child: Image.asset('images/smart_h2r.png', width: 200),
             ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                ),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 20.0,
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(200.0),
                   ),
-                  SizedBox(
-                    height: 60.0,
-                    child: TextField(
-                      controller: userIDController,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        labelText: 'User ID',
-                        hintText: 'Enter your User ID',
-                        border: OutlineInputBorder(),
+                  color: Colors.white,
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 10,
+                      right: 5,
+                      // child: Image.asset('images/smart_h2r.png',
+                      //     width: 200), // Your image goes here
+                      child: RichText(
+                        text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Welcome ',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 55, 145, 214),
+                                  fontSize: 20),
+                            ),
+                            TextSpan(
+                                text: 'to ',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 238, 162, 53),
+                                    fontSize: 20)),
+                            TextSpan(
+                                text: 'SmartH2R',
+                                style: TextStyle(
+                                    color: Color.fromARGB(255, 55, 145, 214),
+                                    fontSize: 20)),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      hintText: 'Enter your Password',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    children: [
-                      Transform.scale(
-                        scale: 0.8,
-                        child: CupertinoSwitch(
-                          value: isChecked,
-                          activeColor: Colors.blue,
-                          onChanged: (bool value) {
-                            setState(() {
-                              isChecked = value;
-                            });
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        SizedBox(
+                          height: 60.0,
+                          child: TextField(
+                            controller: userIDController,
+                            keyboardType: TextInputType.text,
+                            decoration: const InputDecoration(
+                              labelText: 'User ID',
+                              hintText: 'Enter your User ID',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        TextField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            hintText: 'Enter your Password',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        Row(
+                          children: [
+                            Transform.scale(
+                              scale: 0.8,
+                              child: CupertinoSwitch(
+                                value: isChecked,
+                                activeColor: Colors.blue,
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    isChecked = value;
+                                  });
+                                },
+                              ),
+                            ),
+                            const Text('Save Me'),
+                            const Spacer(),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const ForgotPassword(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Forgot Password?',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        ButtonGlobal(
+                          buttontext: 'Sign In',
+                          buttonDecoration: kButtonDecoration.copyWith(
+                              color: const Color.fromARGB(255, 238, 162, 53)),
+                          onPressed: () {
+                            signIn();
                           },
                         ),
-                      ),
-                      Text('Save Me'),
-                      Spacer(),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ForgotPassword(),
-                            ),
-                          );
-                        },
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.blue),
+                        const SizedBox(
+                          height: 20.0,
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      signIn();
-                    },
-                    child: Text('Sign In'),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Don't have an account?"),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SignUp(),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Don't have an account?"),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const SignUp(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                ' Sign Up',
+                                style: TextStyle(color: Colors.blue),
+                              ),
                             ),
-                          );
-                        },
-                        child: Text(
-                          ' Sign Up',
-                          style: TextStyle(color: Colors.blue),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
