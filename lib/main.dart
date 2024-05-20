2ed72a820c8141513b003b5a052afd69a79b104d
JsnSDL <140687658+JsnSDL@users.noreply.github.com>
2024-05-20 17:52:11 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:hrm_employee/providers/attendance_provider.dart';
+import 'package:hrm_employee/providers/attendance_storage_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'Screens/Home/home_screen.dart';
 import 'Screens/Splash Screen/splash_screen.dart';
+import 'providers/user_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
- runApp(
-    ChangeNotifierProvider(
-      create: (_) => UserData(),
-      child: const MyApp(),
-    ),
-  );
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  runApp(MyApp(prefs: prefs));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final SharedPreferences prefs;
+  const MyApp({Key? key, required this.prefs}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        // Add the line below to get horizontal sliding transitions for routes.
-        pageTransitionsTheme: const PageTransitionsTheme(builders: {
-          TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-        }),
-      ),
-      title: 'Maan HRM',
-      home: const SplashScreen(),
-    );
-  }
-}
-
-class UserData extends ChangeNotifier {
-  String? token;
-  String? userID;
-
-  void setUserData(String token, String userID) {
-    this.token = token;
-    this.userID = userID;
-    notifyListeners();
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => AttendanceProvider()),
+          ChangeNotifierProvider(
+              create: (context) => AttendanceStorageProvider()),
+          ChangeNotifierProvider(create: (context) => UserData())
+        ],
+        child: MaterialApp(
+            theme: ThemeData(
+              pageTransitionsTheme: const PageTransitionsTheme(builders: {
+                TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+              }),
+            ),
+            title: 'SmartH2R HRMS',
+            home: const SplashScreen(),
+            routes: <String, WidgetBuilder>{
+              '/homescreen': (BuildContext context) => const HomeScreen(),
+            }));
   }
 }
