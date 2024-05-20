61a6039d3e4c388ac929f5b50eeccb3ae02f3e2e
JsnSDL <140687658+JsnSDL@users.noreply.github.com>
2024-05-20 17:56:54 +0530

    Update on_board.dart

--- lib/Screens/Splash Screen/on_board.dart
+++ lib/Screens/Splash Screen/on_board.dart
@@ -90,161 +90,163 @@ class _OnBoardState extends State<OnBoard> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          Center(
-            child: SizedBox(
-              height: context.height() - 100,
-              width: context.width(),
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  PageView.builder(
-                    itemCount: sliderList.length,
-                    controller: pageController,
-                    onPageChanged: (int index) {
-                      setState(() {
-                        switch (index) {
-                          case 0:
-                            {
-                              percent = 0.33;
-                              currentIndexPage = 0;
-                            }
-                            break;
-                          case 1:
-                            {
-                              percent = 0.66;
-                              currentIndexPage = 1;
-                            }
-                            break;
-                          case 2:
-                            {
-                              percent = 1.00;
-                              currentIndexPage = 2;
-                            }
-                            break;
-                        }
-                      });
-                    },
-                    itemBuilder: (_, index) {
-                      return Column(
-                        children: [
-                          Image.asset(
-                            sliderList[index]['icon'],
-                            fit: BoxFit.fill,
-                            width: context.width(),
-                          ),
-                          Expanded(
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(30.0),
-                                    topRight: Radius.circular(30.0)),
-                                color: Colors.white,
-                              ),
-                              child: Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 20.0,
-                                        right: 30.0,
-                                        top: 15.0,
-                                        bottom: 15.0),
-                                    child: Text(
-                                      sliderList[index]['title'].toString(),
-                                      textAlign: TextAlign.start,
-                                      style: kTextStyle.copyWith(
-                                        fontSize: 25.0,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        left: 20.0, right: 20.0),
-                                    // ignore: sized_box_for_whitespace
-                                    child: Container(
-                                      width: context.width(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: SizedBox(
+                height: context.height() - 100,
+                width: context.width(),
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    PageView.builder(
+                      itemCount: sliderList.length,
+                      controller: pageController,
+                      onPageChanged: (int index) {
+                        setState(() {
+                          switch (index) {
+                            case 0:
+                              {
+                                percent = 0.33;
+                                currentIndexPage = 0;
+                              }
+                              break;
+                            case 1:
+                              {
+                                percent = 0.66;
+                                currentIndexPage = 1;
+                              }
+                              break;
+                            case 2:
+                              {
+                                percent = 1.00;
+                                currentIndexPage = 2;
+                              }
+                              break;
+                          }
+                        });
+                      },
+                      itemBuilder: (_, index) {
+                        return Column(
+                          children: [
+                            Image.asset(
+                              sliderList[index]['icon'],
+                              fit: BoxFit.fill,
+                              width: context.width(),
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(30.0),
+                                      topRight: Radius.circular(30.0)),
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 20.0,
+                                          right: 30.0,
+                                          top: 15.0,
+                                          bottom: 15.0),
                                       child: Text(
-                                        sliderList[index]['description']
-                                            .toString(),
+                                        sliderList[index]['title'].toString(),
                                         textAlign: TextAlign.start,
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 5,
                                         style: kTextStyle.copyWith(
-                                          fontSize: 15.0,
-                                          color: kGreyTextColor,
+                                          fontSize: 25.0,
+                                          fontWeight: FontWeight.bold,
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 20.0, right: 20.0),
+                                      // ignore: sized_box_for_whitespace
+                                      child: Container(
+                                        width: context.width(),
+                                        child: Text(
+                                          sliderList[index]['description']
+                                              .toString(),
+                                          textAlign: TextAlign.start,
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 5,
+                                          style: kTextStyle.copyWith(
+                                            fontSize: 15.0,
+                                            color: kGreyTextColor,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
+                            // ignore: sized_box_for_whitespace
+                          ],
+                        );
+                      },
+                    ),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: DotIndicator(
+                            currentDotSize: 15,
+                            dotSize: 6,
+                            pageController: pageController,
+                            pages: sliderList,
+                            indicatorColor: kMainColor,
+                            unselectedIndicatorColor: Colors.grey,
                           ),
-                          // ignore: sized_box_for_whitespace
-                        ],
-                      );
-                    },
-                  ),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: DotIndicator(
-                          currentDotSize: 15,
-                          dotSize: 6,
-                          pageController: pageController,
-                          pages: sliderList,
-                          indicatorColor: kMainColor,
-                          unselectedIndicatorColor: Colors.grey,
                         ),
-                      ),
-                      const Spacer(),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: CircularPercentIndicator(
-                          radius: 80.0,
-                          lineWidth: 3.0,
-                          progressColor: kMainColor,
-                          percent: percent,
-                          animation: true,
-                          center: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                currentIndexPage < 2
-                                    ? pageController.nextPage(
-                                        duration:
-                                            const Duration(microseconds: 3000),
-                                        curve: Curves.bounceInOut)
-                                    : Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const SelectType()),
-                                      );
-                              });
-                            },
-                            child: const CircleAvatar(
-                              radius: 35.0,
-                              backgroundColor: kMainColor,
-                              child: Center(
-                                child: Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.white,
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircularPercentIndicator(
+                            radius: 80.0,
+                            lineWidth: 3.0,
+                            progressColor: kMainColor,
+                            percent: percent,
+                            animation: true,
+                            center: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  currentIndexPage < 2
+                                      ? pageController.nextPage(
+                                          duration: const Duration(
+                                              microseconds: 3000),
+                                          curve: Curves.bounceInOut)
+                                      : Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const SelectType()),
+                                        );
+                                });
+                              },
+                              child: const CircleAvatar(
+                                radius: 35.0,
+                                backgroundColor: kMainColor,
+                                child: Center(
+                                  child: Icon(
+                                    Icons.arrow_forward_ios,
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
