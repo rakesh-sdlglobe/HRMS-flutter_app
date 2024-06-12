096a8def1c4a164eeb8066f5eb9a73d6dc248020
JsnSDL <140687658+JsnSDL@users.noreply.github.com>
2024-06-12 16:26:27 +0530

    Update leaveRoute.js

--- routes/leaveRoute.js
+++ routes/leaveRoute.js
@@ -7,5 +7,8 @@ const leaveController = require('../controllers/leaveController');
 // Route for handling login requests
 router.post('/apply', isAuth,  leaveController.insertLeave);
 router.post('/get', isAuth,  leaveController.fetchLeave);
+router.post('/approveGet', isAuth, leaveController.leaveToApprove)
+router.post('/approve', isAuth, leaveController.leaveApprove)
+
 
 module.exports = router;
