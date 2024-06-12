b3ad2724b9d3f644d83ed73fb82d69850d919584
JsnSDL <140687658+JsnSDL@users.noreply.github.com>
2024-06-12 16:22:36 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const router = express.Router();
+const isAuth = require("../middleware/isAuth")
 const authController = require('../controllers/authController');
 
 // Route for handling login requests
 router.post('/login', authController.login);
+router.post('/getUser', isAuth, authController.getUser);
+
 
 module.exports = router;
